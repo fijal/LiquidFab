39b8e0eb4e68855edc4736024c28b40ed7403574
Maciej Fijalkowski <fijall@gmail.com>
2026-06-06 05:38:44 +0200

    fixup terrain to approximate at edges too

--- Assets/Resources/Terrain/tiling.py
+++ Assets/Resources/Terrain/tiling.py
@@ -11,14 +11,16 @@ from scipy.ndimage import zoom
 im_frame = Image.open('terrain.png')
 OUT_SIZE = 1024
 
-def get_tile(ofs_x, ofs_y, size, out_size):
+def get_tile(v, ofs_x, ofs_y, size, out_size):
     max_h = np.array(im_frame.get_flattened_data()).max()
-    img = np.array(im_frame.get_flattened_data()).reshape((512, 512))[ofs_x:ofs_x + size, ofs_y:size + ofs_y]
+    # img = np.array(im_frame.get_flattened_data()).reshape((512, 512))[ofs_x:ofs_x + size, ofs_y:size + ofs_y]
     
-    grid = np.arange(0, size, dtype=np.int32)
-    rekd = RectBivariateSpline(grid, grid, img)
-    igrid = np.linspace(0, size, out_size)
-    v = rekd(igrid, igrid)
+    # grid = np.arange(0, size, dtype=np.int32)
+    # rekd = RectBivariateSpline(grid, grid, img)
+    # igrid = np.linspace(0, size, out_size)
+    # v = rekd(igrid, igrid)
+
+    v = v[ofs_x:ofs_x + size, ofs_y:ofs_y + size]
     
     x = np.arange(out_size)
     x, y = np.meshgrid(x, x)
@@ -27,10 +29,17 @@ def get_tile(ofs_x, ofs_y, size, out_size):
     return v
 
 TILE_SIZE = 32
+
+img = np.array(im_frame.get_flattened_data()).reshape((512, 512))
+grid = np.arange(0, 512, dtype=np.int32)
+rekd = RectBivariateSpline(grid, grid, img)
+igrid = np.linspace(0, 512, int(512 * (128 / TILE_SIZE)))
+v = rekd(igrid, igrid)
+
 for x in range(int(512 / TILE_SIZE)):
     for y in range(int(512 / TILE_SIZE)):
-        v = get_tile(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, 128)
-        Image.fromarray(v, mode='L').save('tile%d_%d.png' % (x, y))
+        o = get_tile(v, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, 128)
+        Image.fromarray(o, mode='L').save('tile%d_%d.png' % (x, y))
 
 #img = Image.fromarray(v.flatten().astype(np.float32))
 #img.save('foo.png')
