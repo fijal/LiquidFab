cd5f5f416513a08be78c99351243e853dab7d02e
Maciej Fijalkowski <fijall@gmail.com>
2026-06-06 07:22:19 +0200

    update terrain tiling

--- Assets/Terrain.cs
+++ Assets/Terrain.cs
@@ -5,47 +5,59 @@ using UnityEngine;
 public class Terrain : MonoBehaviour
 {
     Mesh mesh;
-    int TILE_SIZE_X = 128;
-    int TILE_SIZE_Y = 128;
+    int TILE_SIZE_X = 129;
+    int TILE_SIZE_Y = 129;
     int TILES = 16;
-    float scale = 0.5f;
+    const float SCALE = 0.5f;
+    const float HEIGHT_SCALE = 40f;
+
+    public Material terrainMat;
     
     // Start is called before the first frame update
     void Start()
     {
         for (int x = 0; x < TILES; x++)
             for (int y = 0; y < TILES; y++)
-                createTile(x, y);
+            {
+                var tile = new GameObject($"tile{x}_{y}");
+                tile.transform.SetParent(transform);
+                tile.transform.position = new Vector3(TILE_SIZE_X * x * SCALE, 0, TILE_SIZE_Y * y * SCALE);
+                tile.transform.rotation = Quaternion.Euler(new Vector3(0, 270, 0));
+                var r = tile.AddComponent<MeshRenderer>();
+                r.material = terrainMat;
+                var filter = tile.AddComponent<MeshFilter>();
+                filter.mesh = createTile(x, y);
+            }
     }
 
     Mesh createTile(int ofsX, int ofsY)
     {
+        Debug.Log($"{ofsX} {ofsY}");
         mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
         var map = Resources.Load($"terrain/tile{ofsX}_{ofsY}") as Texture2D;
         
-        var vertices = new Vector3[(TILE_SIZE_X + 1) * (TILE_SIZE_Y + 1)];
-        var triangles = new int[TILE_SIZE_X * TILE_SIZE_Y * 6];
+        var vertices = new Vector3[(TILE_SIZE_X) * (TILE_SIZE_Y)];
+        var triangles = new int[(TILE_SIZE_X - 1) * (TILE_SIZE_Y - 1) * 6];
         var height = map.GetPixels();
-
-        for (int x = 0; x < TILE_SIZE_X + 1; ++x)
-            for (int y = 0; y < TILE_SIZE_Y + 1; ++y)
+        
+        for (int x = 0; x < TILE_SIZE_X; ++x)
+            for (int y = 0; y < TILE_SIZE_Y; ++y)
             {
-                var h = height[x + y * 512].r * 10;
-                vertices[x + y * (TILE_SIZE_X + 1)] = new Vector3(x * scale, h, y * scale);
+                var h = height[x + y * TILE_SIZE_X].r * HEIGHT_SCALE * SCALE;
+                vertices[x + y * (TILE_SIZE_X)] = new Vector3(x * SCALE, h, y * SCALE);
             }
         int vert = 0;
-        for (int tris = 0; tris < TILE_SIZE_X * TILE_SIZE_Y; tris++)
+        for (int tris = 0; tris < (TILE_SIZE_X - 1) * (TILE_SIZE_Y - 1); tris++)
         {
             var t = tris * 6;
             triangles[t + 0] = vert + 0;
-            triangles[t + 1] = vert + TILE_SIZE_X + 1;
+            triangles[t + 1] = vert + TILE_SIZE_X;
             triangles[t + 2] = vert + 1;
             triangles[t + 3] = vert + 1;
-            triangles[t + 4] = vert + TILE_SIZE_X + 1;
-            triangles[t + 5] = vert + TILE_SIZE_X + 2;
+            triangles[t + 4] = vert + TILE_SIZE_X;
+            triangles[t + 5] = vert + TILE_SIZE_X + 1;
             vert++;
-            if (tris % TILE_SIZE_X == TILE_SIZE_X - 1)
+            if (tris % (TILE_SIZE_X - 1) == TILE_SIZE_X - 2)
                 vert++;
         }
         mesh.vertices = vertices;
