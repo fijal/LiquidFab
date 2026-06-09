894da0bc7e0779810183513c49c964432fce73e3
Maciej Fijalkowski <fijall@gmail.com>
2026-06-09 17:29:42 +0200

    (fijal, arigo) Fix the water flow

--- Assets/Water.cs
+++ Assets/Water.cs
@@ -7,6 +7,10 @@ public class Water : MonoBehaviour
     float[] waterLevel;
     float[] waterLevelStep;
     float[] waterLevelPrevStep;
+
+    float[] flowX;
+    float[] flowY;
+
     public TextAsset terrainData;
     byte[] terrainDataBytes;
 
@@ -23,15 +27,17 @@ public class Water : MonoBehaviour
         terrainDataBytes = terrainData.bytes;
 
         waterLevel = new float[WATER_SIZE_X * WATER_SIZE_Y];
+        flowX = new float[WATER_SIZE_X * WATER_SIZE_Y];
+        flowY = new float[WATER_SIZE_X * WATER_SIZE_Y];
         waterLevelStep = new float[WATER_SIZE_X * WATER_SIZE_Y];
         waterLevelPrevStep = new float[WATER_SIZE_X * WATER_SIZE_Y];
         
         for (int y = 0; y < WATER_SIZE_Y; y++)
             for (int x = 0; x < WATER_SIZE_X; x++)
             {
-                waterLevel[x + y * WATER_SIZE_X] = -1;
-                waterLevelStep[x + y * WATER_SIZE_X] = -1;
-                waterLevelPrevStep[x + y * WATER_SIZE_X] = -1;
+                waterLevel[x + y * WATER_SIZE_X] = 0; // -1;
+                waterLevelStep[x + y * WATER_SIZE_X] = 0; // -1;
+                waterLevelPrevStep[x + y * WATER_SIZE_X] = 0; // -1;
             }
         for (int x = 0; x < 10; x++)
         {
@@ -66,8 +72,8 @@ public class Water : MonoBehaviour
     float wlT(int x, int y)
     {
         var c = wl(x, y);
-        if (c == -1)
-            c = 0;
+        //if (c == -1)
+        //    c = 0;
         return c + terrainHeight(x, y);
     }
 
@@ -76,117 +82,62 @@ public class Water : MonoBehaviour
         waterLevel[x + y * WATER_SIZE_X] = val;
     }
 
-    int calculateVertexCount(int[] offsets, int[] relOffsets)
-    {
-        for (int y = 0; y < WATER_SIZE_Y; y++)
-            offsets[y] = -1;
-
-        int vert = 0;
-        for (int y = 0; y < WATER_SIZE_Y; ++y)
-        {
-            for (int x = 0; x < WATER_SIZE_X; ++x)
-            {
-                if (waterLevel[x + y * WATER_SIZE_X] >= 0)
-                {
-                    if (offsets[y] == -1)
-                    {
-                        relOffsets[y] = x;
-                        offsets[y] = vert;
-                    }
-                    vert++;
-                }
-            }
-        }
-        return vert;
-    }
-
-    void populateVertices(Vector3[] vert)
-    {
-        int c = 0;
-        for (int y = 0; y < WATER_SIZE_Y; ++y)
-            for (int x = 0; x < WATER_SIZE_X; ++x)
-                if (wl(x, y) >= 0)
-                {
-                    vert[c] = new Vector3(x * Terrain.SCALE, wl(x, y) + terrainHeight(x, y), y * Terrain.SCALE);
-                    c++;
-                }
-    }
-
-    void populateTriangles(int[] triangles, int[] offsets, int[] relOffsets)
-    {
-        var t = 0;
-        for (int y = 1; y < WATER_SIZE_Y - 1; ++y)
-        {
-            for (int x = 1; x < WATER_SIZE_X - 1; ++x)
-            {
-                if (wl(x, y) >= 0 && wl(x + 1, y) >= 0 && wl(x, y + 1) >= 0 && wl(x + 1, y + 1) >= 0)
-                {
-                    triangles[t + 2] = offsets[y] + x - relOffsets[y];
-                    triangles[t + 1] = offsets[y] + x + 1 - relOffsets[y];
-                    triangles[t + 0] = offsets[y + 1] + x - relOffsets[y + 1];
-                    triangles[t + 3] = offsets[y] + x + 1 - relOffsets[y];
-                    triangles[t + 4] = offsets[y + 1] + x - relOffsets[y + 1];
-                    triangles[t + 5] = offsets[y + 1] + x + 1 - relOffsets[y + 1];
-                    t += 6;
-                    continue;
-                }
-                if (wl(x, y) >= 0 && wl(x + 1, y) >= 0 && wl(x, y + 1) >= 0)
-                {
-                    triangles[t + 2] = offsets[y] + x - relOffsets[y];
-                    triangles[t + 1] = offsets[y] + x + 1 - relOffsets[y];
-                    triangles[t + 0] = offsets[y + 1] + x - relOffsets[y + 1];
-                    t += 3;
-                    continue;
-                }
-                if (wl(x + 1, y) >= 0 && wl(x + 1, y + 1) >= 0 && wl(x, y + 1) >= 0)
-                {
-                    triangles[t + 2] = offsets[y] + x + 1 - relOffsets[y];
-                    triangles[t + 1] = offsets[y + 1] + x + 1 - relOffsets[y + 1];
-                    triangles[t + 0] = offsets[y + 1] + x - relOffsets[y + 1];
-                    t += 3;
-                    continue;
-                }
-                if (wl(x, y) >= 0 && wl(x + 1, y) >= 0 && wl(x + 1, y + 1) >= 0)
-                {
-                    triangles[t + 2] = offsets[y] + x - relOffsets[y];
-                    triangles[t + 1] = offsets[y] + x + 1 - relOffsets[y];
-                    triangles[t + 0] = offsets[y + 1] + x + 1 - relOffsets[y + 1];
-                    t += 3;
-                    continue;
-                }
-                if (wl(x, y) >= 0 && wl(x, y + 1) >= 0 && wl(x + 1, y + 1) >= 0)
-                {
-                    triangles[t + 0] = offsets[y] + x - relOffsets[y];
-                    triangles[t + 1] = offsets[y + 1] + x - relOffsets[y + 1];
-                    triangles[t + 2] = offsets[y + 1] + x + 1 - relOffsets[y + 1];
-                    t += 3;
-                    continue;
-                }
-            }
-        }
-    }
-
-    int calculateTriangleCount()
-    {
-        
-        int tris = 0;
-        for (int y = 1; y < WATER_SIZE_Y - 1; ++y)
-            for (int x = 1; x < WATER_SIZE_X - 1; ++x)
-            {
-                var v = (((wl(x, y) >= 0) ? 1 : 0) +
-                         ((wl(x + 1, y) >= 0) ? 1 : 0) +
-                         ((wl(x, y + 1) >= 0) ? 1 : 0) +
-                         ((wl(x + 1, y + 1) >= 0) ? 1 : 0));
-                if (v == 4)
-                    tris += 2;
-                else if (v == 3)
-                    tris++;
-            }
-        return tris;
-    }
-
     void moveWater()
     {
+        const float FC = 0.003f;
+
+        float flowx(int x, int y)
+        {
+            return flowX[x + y * WATER_SIZE_X];
+        }
+
+        float flowy(int x, int y)
+        {
+            return flowY[x + y * WATER_SIZE_X];
+        }
+
+        float zeroclip(float f)
+        {
+            return f < 0 ? 0 : f;
+        }
+
+        const float dt = 1f;
+
+        for (int y = 1; y < WATER_SIZE_Y - 1; y++)
+            for (int x = 1; x < WATER_SIZE_X - 1; x++)
+            {
+                flowX[x + y * WATER_SIZE_X] += FC * dt * (wlT(x - 1, y) - wlT(x, y));
+                flowY[x + y * WATER_SIZE_X] += FC * dt * (wlT(x, y - 1) - wlT(x, y));
+            }
+
+        for (int y = 1; y < WATER_SIZE_Y - 1; y++)
+            for (int x = 1; x < WATER_SIZE_X - 1; x++)
+            {
+                float total = zeroclip(-flowx(x, y)) + zeroclip(-flowy(x, y)) + zeroclip(flowx(x + 1, y)) + zeroclip(flowy(x, y + 1));
+                float max_outflow = wl(x, y) / dt;
+                if (total > 0)
+                {
+                    var scale = Mathf.Min(1f, max_outflow / total);
+                    if (flowx(x, y) < 0)
+                        flowX[x + y * WATER_SIZE_X] *= scale;
+                    if (flowy(x, y) < 0)
+                        flowY[x + y * WATER_SIZE_X] *= scale;
+                    if (flowx(x + 1, y) > 0)
+                        flowX[x + 1 + y * WATER_SIZE_X] *= scale;
+                    if (flowy(x, y + 1) > 0)
+                        flowY[x + (y + 1) * WATER_SIZE_X] *= scale;
+
+                }
+            }
+
+        for (int y = 1; y < WATER_SIZE_Y - 1; y++)
+            for (int x = 1; x < WATER_SIZE_X - 1; x++)
+            {
+                waterLevel[x + y * WATER_SIZE_X] += (flowx(x, y) + flowy(x, y) - flowx(x + 1, y) - flowy(x, y + 1)) / dt;
+            }
+
+
+        return;
         for (int y = 1; y < WATER_SIZE_Y - 1; y++)
             for (int x = 1; x < WATER_SIZE_X - 1; x++)
             {
@@ -204,9 +155,22 @@ public class Water : MonoBehaviour
                 waterLevelStep[x + y * WATER_SIZE_X] = 2 * cur + alpha * diff - prev;
             }
 
+        for (int y = 1; y < WATER_SIZE_Y - 1; y++)
+            for (int x = 1; x < WATER_SIZE_X - 1; x++)
+            {
+                var epsilon = 0.1f;
+                /*if ((waterLevelStep[(x + 1) + y * WATER_SIZE_X] <= epsilon) &&
+                    (waterLevelStep[(x) + (y + 1) * WATER_SIZE_X] <= epsilon) &&
+                    (waterLevelStep[(x - 1) + y * WATER_SIZE_X] <= epsilon) &&
+                    (waterLevelStep[(x) + (y - 1) * WATER_SIZE_X] <= epsilon))
+                    waterLevelStep[x + y * WATER_SIZE_X] = -1;*/
+            }
+
+
         for (int y = 1; y < WATER_SIZE_Y; y++)
             for (int x = 1; x < WATER_SIZE_X; x++)
                 waterLevelPrevStep[x + y * WATER_SIZE_X] = waterLevel[x + y * WATER_SIZE_X];
+
         float[] b = waterLevel;
         waterLevel = waterLevelStep;
         waterLevelStep = b;
@@ -217,51 +181,40 @@ public class Water : MonoBehaviour
         int start = 1, end = 199;
         int size = end - start;
         var vertices = new Vector3[size * size];
-        var tris = new int[(size - 1) * (size - 1) * 6];
+        var tris = new List<int>();
+
+        bool visible(int x, int y)
+        {
+            return wl(x, y) > 0.01f;
+        }
 
         int c = 0;
-        int t = 0;
         for (int y = start; y < end; y++)
             for (int x = start; x < end; x++)
             {
-                vertices[c] = new Vector3(x * Terrain.SCALE, wl(x, y) + terrainHeight(x, y), y * Terrain.SCALE);
+                vertices[c] = new Vector3(x * Terrain.SCALE, wl(x, y) + terrainHeight(x, y) - 0.001f, y * Terrain.SCALE);
                 if (y < end - 1 && x < end - 1)
+                    //&& (visible(x, y) || visible(x, y + 1) || visible(x + 1, y) || visible(x + 1, y + 1)))
                 {
                     int ix = x - start;
                     int iy = y - start;
-                    tris[t] = ix + iy * size;
-                    tris[t + 1] = ix + (iy + 1) * size;
-                    tris[t + 2] = (ix + 1) + (iy + 1) * size;
-                    tris[t + 3] = ix + iy * size;
-                    tris[t + 4] = (ix + 1) + (iy + 1) * size;
-                    tris[t + 5] = ix + 1 + iy * size;
-                    t += 6;
+                    tris.Add(ix + iy * size);
+                    tris.Add(ix + (iy + 1) * size);
+                    tris.Add((ix + 1) + (iy + 1) * size);
+                    tris.Add(ix + iy * size);
+                    tris.Add((ix + 1) + (iy + 1) * size);
+                    tris.Add(ix + 1 + iy * size);
                 }
                 c++;
             }
 
         var mesh = GetComponent<MeshFilter>().mesh;
         mesh.vertices = vertices;
-        mesh.triangles = tris;
+        mesh.SetTriangles(tris, 0);
         mesh.RecalculateBounds();
         mesh.RecalculateNormals();
 
         return;
-        /*int[] offsets = new int[WATER_SIZE_Y];
-        int[] relOffsets = new int[WATER_SIZE_Y];
-        var vCount = calculateVertexCount(offsets, relOffsets);
-        var vertices = new Vector3[vCount];
-        populateVertices(vertices);
-        var tris = new int[calculateTriangleCount() * 6];
-        populateTriangles(tris, offsets, relOffsets);
-
-        var mesh = GetComponent<MeshFilter>().mesh;
-        mesh.vertices = vertices;
-        mesh.triangles = tris;
-        mesh.RecalculateBounds();
-        mesh.RecalculateNormals();
-        
-        return;*/
     }
 
     // Update is called once per frame
