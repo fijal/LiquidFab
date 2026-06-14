49aab8965ca725347c4fa6df5ce4a76e94b3f519
Armin Rigo <armin.rigo@gmail.com>
2026-06-14 18:43:39 +0200

    better highlight for the selected tool

--- Assets/ToolbarItem.cs
+++ Assets/ToolbarItem.cs
@@ -5,17 +5,19 @@ using UnityEngine.UI;
 
 public class ToolbarItem : MonoBehaviour
 {
-    public Sprite gray, color;
+    public Sprite gray, color, frame, frameActive;
     public string helperText;
     public ToolSelected tool;
 
     public void activate()
     {
         GetComponent<Image>().sprite = color;
+        transform.Find("Frame").GetComponent<Image>().sprite = frameActive;
     }
 
     public void deactivate()
     {
         GetComponent<Image>().sprite = gray;
+        transform.Find("Frame").GetComponent<Image>().sprite = frame;
     }
 }
