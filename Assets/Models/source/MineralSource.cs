a735b79c78d91e9357d48fc1b82f67c24c04d12d
Maciej Fijalkowski <fijall@gmail.com>
2026-07-29 16:22:03 +0200

    Increase the chances a bit

--- Assets/Models/source/MineralSource.cs
+++ Assets/Models/source/MineralSource.cs
@@ -21,10 +21,10 @@ public class MineralSource : MonoBehaviour
             var r = Random.Range(0f, 1f);
             var cur = transform.position;
             cur = new Vector3(cur.x, terrain.heightFloat(cur.x / Terrain.SCALE, cur.z / Terrain.SCALE), cur.z);
-            if (r < 0.05f)
+            if (r < 0.1f)
             {
                 terrain.spawnFloater(cur, iron, ItemType.Iron);
-            } else if (r < 0.1f)
+            } else if (r < 0.2f)
             {
                 terrain.spawnFloater(cur, copper, ItemType.Copper);
             }
