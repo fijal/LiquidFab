47b79de850ba5eb77af87b251667f539820c0e79
Maciej Fijalkowski <fijall@gmail.com>
2026-09-18 13:05:52 +0200

    Update

--- Assets/tmp/particles/Particles.cs
+++ Assets/tmp/particles/Particles.cs
@@ -9,6 +9,7 @@ public class Particles : MonoBehaviour
     float[] attraction;
 
     static int nParticles = 1024 * 20;
+    static int nColors = 6;
     static int textureSize = 2048;
     ComputeBuffer partBuf, colorBuf, attrBuf;
 
@@ -18,6 +19,7 @@ public class Particles : MonoBehaviour
     static int _TimeID = Shader.PropertyToID("_Time");
     static int _attractionID = Shader.PropertyToID("attraction");
     static int _textureSizeID = Shader.PropertyToID("textureSize");
+    static int _nColorsID = Shader.PropertyToID("nColors");
     
     [StructLayout(LayoutKind.Sequential)]
     struct Particle
@@ -31,11 +33,12 @@ public class Particles : MonoBehaviour
 
     void Start()
     {
-        colors = new Vector4[4] { new Vector4(1, 0, 0, 1), new Vector4(0, 1, 0, 1), new Vector4(0, 0, 1, 1), new Vector4(1, 1, 0, 1)};
+        colors = new Vector4[6] { new Vector4(1, 0, 0, 1), new Vector4(0, 1, 0, 1), new Vector4(0, 0, 1, 1), new Vector4(1, 1, 0, 1),
+                                  new Vector4(0, 1, 1, 1), new Vector4(1, 0, 1, 1)};
         particles = new Particle[nParticles];
-        attraction = new float[4 * 4];
+        attraction = new float[nColors * nColors];
 
-        for (int i = 0; i < 4 * 4; i++)
+        for (int i = 0; i < nColors * nColors; i++)
         {
             attraction[i] = Random.Range(-1.0f, 1.0f);
         }
@@ -49,13 +52,13 @@ public class Particles : MonoBehaviour
             particles[i].position = new Vector2(
                 Random.Range(0f, (float)textureSize),
                 Random.Range(0f, (float)textureSize));
-            particles[i].kind = Random.Range(0, 4);
+            particles[i].kind = Random.Range(0, nColors);
             particles[i].speed = new Vector2(Random.Range(-2f, 2f), Random.Range(-2f, 2f));
         }
 
         partBuf = new ComputeBuffer(nParticles, 4 * (2 + 2 + 1));
         colorBuf = new ComputeBuffer(colors.Length, 4 * 4);
-        attrBuf = new ComputeBuffer(4 * 4, 4);
+        attrBuf = new ComputeBuffer(nColors * nColors, 4);
 
         partBuf.SetData(particles);
         colorBuf.SetData(colors);
@@ -64,11 +67,13 @@ public class Particles : MonoBehaviour
         shader.SetInt(_textureSizeID, textureSize);
         shader.SetTexture(0, _ResultID, GetComponent<RawImage>().mainTexture);
         shader.SetBuffer(0, _colorsID, colorBuf);
+        shader.SetInt(_nColorsID, nColors);
         clear.SetTexture(0, _ResultID, GetComponent<RawImage>().mainTexture);
         clear.Dispatch(0, textureSize / 8, textureSize / 8, 1);
         simulationStep.SetBuffer(0, _ParticlesID, partBuf);
         simulationStep.SetBuffer(0, _attractionID, attrBuf);
         simulationStep.SetInt(_textureSizeID, textureSize);
+        simulationStep.SetInt(_nColorsID, nColors);
     }
 
     void Update()
